c606c03a24fe6a60c6c4d47919ae8d63c2a993a3
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-18 16:31:35 +0300

    Update task9.js

--- task9.js
+++ task9.js
@@ -1,1 +1,13 @@
+let funcs = [];
 
+for (let i = 0; i < 3; i++) {
+    funcs.push((function(index) {
+        return function() {
+            console.log(index);
+        };
+    })(i));
+}
+
+// Вивід:
+funcs[0](); // 0
+funcs[2](); // 2
