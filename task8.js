08c2f8857fe2feedc68218b08c695ad781083f8c
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-18 16:31:10 +0300

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,1 +1,19 @@
-
+function createArrayIterator(array) {
+ }
+ 
+ var arr = [5, 3, 7];
+ var itr = createArrayIterator(arr);
+ console.log(itr()); // 5
+ console.log(itr()); // 3
+ console.log(itr()); // 7
+ console.log(itr()); // undefined
+ function createArrayIterator(array) {
+    let index = 0;
+    return function() {
+        if (index < array.length) {
+            return array[index++];
+        } else {
+            return undefined;
+        }
+    };
+}
