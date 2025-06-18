352b58043c053d55814c2b483fe33c59dc14de21
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-18 16:30:40 +0300

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,1 +1,18 @@
+function createRectangle(width, height) {
+    function isValid(value) {
+        return typeof value === 'number' && value > 0;
+    }
 
+    if (!isValid(width) || !isValid(height)) {
+        throw new Error("Невірні значення сторін.");
+    }
+
+    return {
+        getPerimeter: function() {
+            return 2 * (width + height);
+        },
+        getArea: function() {
+            return width * height;
+        }
+    };
+}
