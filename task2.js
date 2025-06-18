708abb269c1c3988feea5dd77151dfdfd358c2b1
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-18 16:27:22 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,1 +1,8 @@
-
+function min(...args) {
+    if (args.length === 0) return undefined;
+    let result = args[0];
+    for (let num of args) {
+        if (num < result) result = num;
+    }
+    return result;
+}
